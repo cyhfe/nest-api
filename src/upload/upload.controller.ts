9e94dcb0c2fe3665a068f673751dd138451c688f
chenyuhao <cccchenyuhao@gmail.com>
2023-12-16 00:35:31 +0800

    upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -6,14 +6,25 @@ import {
   UploadedFiles,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
-import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
+import {
+  AnyFilesInterceptor,
+  FileInterceptor,
+  MulterModule,
+} from '@nestjs/platform-express';
+
+MulterModule.register({
+  dest: './upload',
+});
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
   @Post()
-  @UseInterceptors(AnyFilesInterceptor())
+  @UseInterceptors(AnyFilesInterceptor({ dest: 'upload/' }))
   uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
     console.log(files);
+    // return {
+    //   files: files[0].buffer.toString(),
+    // };
   }
 }
