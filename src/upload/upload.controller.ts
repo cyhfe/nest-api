a9d57a710f9c43ce7c078da1763eb9e1816b4fb5
chenyuhao <cccchenyuhao@gmail.com>
2023-12-16 14:36:56 +0800

    update

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -50,11 +50,8 @@ export class UploadController {
         filename: (req, file, callback) => {
           const name = file.originalname.split('.')[0];
           const fileExtName = extname(file.originalname);
-          const randomName = Array(4)
-            .fill(null)
-            .map(() => Math.round(Math.random() * 16).toString(16))
-            .join('');
-          callback(null, `${name}-${randomName}${fileExtName}`);
+
+          callback(null, `${name}-${Date.now()}${fileExtName}`);
         },
       }),
     }),
